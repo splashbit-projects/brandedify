4b5c8aa336bebcbbe4ac412bfaf01063b5779cce
SergWebAcceptance <serg.web@acceptance.ltd>
2024-12-12 17:23:40 +0200

    added mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,104 @@
 "use client";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 import "@/styles/header.scss";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
   const pathname = usePathname();
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const menuOpen = () => {
+    setMenuOpened(!menuOpened);
+    document.body.classList.toggle("no-scroll", !menuOpened);
+  };
+
+  const closeMenu = () => {
+    setMenuOpened(false);
+    setButtonActive(false);
+    document.body.classList.remove("no-scroll");
+  };
 
   return (
-    <header>
-      <div className="_container">
-        <div className="row">
-          <Link href="/">
-            <img src="/images/Logo.svg" alt="Logo" />
-          </Link>
+    <>
+      <header>
+        <div className="_container">
+          <div className="row">
+            <Link href="/">
+              <img src="/images/Logo.svg" alt="Logo" />
+            </Link>
+            <nav>
+              <Link href="/" className={pathname === "/" ? "current" : ""}>
+                Home
+              </Link>
+              <Link
+                href="/agency"
+                className={pathname === "/agency" ? "current" : ""}
+              >
+                Agency
+              </Link>
+              <Link
+                href="/services"
+                className={pathname === "/services" ? "current" : ""}
+              >
+                Services
+              </Link>
+              <Link
+                href="/solutions"
+                className={pathname === "/solutions" ? "current" : ""}
+              >
+                Solutions
+              </Link>
+              <Link
+                href="/results"
+                className={pathname === "/results" ? "current" : ""}
+              >
+                Results
+              </Link>
+              <Link
+                href="/resources"
+                className={pathname === "/resources" ? "current" : ""}
+              >
+                Resources
+              </Link>
+              <Link
+                href="/pricing"
+                className={pathname === "/pricing" ? "current" : ""}
+              >
+                Pricing
+              </Link>
+              <Link
+                href="/faq"
+                className={pathname === "/faq" ? "current" : ""}
+              >
+                FAQ
+              </Link>
+              <Link
+                href="/contact"
+                className={pathname === "/contact" ? "current" : ""}
+              >
+                Contact
+              </Link>
+            </nav>
+            <Link href="#" className="head-button">
+              Get started
+            </Link>
+            <button
+              onClick={menuOpen}
+              className={`menu-btn ${menuOpened ? "active" : ""}`}
+            >
+              {!menuOpened ? (
+                <img src="/images/menu.svg" alt="menu-burger" />
+              ) : (
+                <img alt="logo" src="/images/menu-close.svg" />
+              )}
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <div className={`menu-wrap ${menuOpened ? "active" : ""}`}>
+        <div className="_container">
           <nav>
             <Link href="/" className={pathname === "/" ? "current" : ""}>
               Home
@@ -54,10 +139,7 @@ const Header = () => {
             >
               Pricing
             </Link>
-            <Link
-              href="/faq"
-              className={pathname === "/faq" ? "current" : ""}
-            >
+            <Link href="/faq" className={pathname === "/faq" ? "current" : ""}>
               FAQ
             </Link>
             <Link
@@ -67,12 +149,13 @@ const Header = () => {
               Contact
             </Link>
           </nav>
-          <Link href="#" className="head-button">
-            Get started
+          <Link href="#" className="main-button">
+            <span>Get started</span>
+            <img src="/images/arrow.svg" />
           </Link>
         </div>
       </div>
-    </header>
+    </>
   );
 };
 
