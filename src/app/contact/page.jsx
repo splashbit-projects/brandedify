bd2a76db7997ee10cf9f26af9e24013081a8ea95
SergWebAcceptance <serg.web@acceptance.ltd>
2024-12-13 15:23:52 +0200

    added meta to contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import "@/styles/contact.scss";
 import ContactBlock from "./_components/ContactBlock";
 
+
+export const metadata = {
+    title: "Contact Brandedify | Let's Grow Your Brand",
+    description:
+      "Contact Brandedify for personalized digital marketing solutions. You can reach us via email, phone, or our contact form.",
+    openGraph: {
+      title: "Contact Brandedify | Let's Grow Your Brand",
+      description:
+        "Contact Brandedify for personalized digital marketing solutions. You can reach us via email, phone, or our contact form.",
+      //images: "",
+    },
+  };
+  
+
 const Contact = () => {
   return (
     <>
